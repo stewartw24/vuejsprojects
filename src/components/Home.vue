b296dbb882ae2aa1531351e2484c1e5039d43898
William <stewart-w24@hotmail.co.uk>
2021-06-24 22:50:51 +0100

    adding old vue2 projects to github

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Trade or view your portfolio</h1>
+        <h6>You may save and load your data</h6>
+        <h6>Click on 'End Day' to begin a new day</h6>
+        <hr>
+        <p>Your funds: {{ funds | currency }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        funds() {
+            return this.$store.getters.funds;
+        }
+    }
+}
+</script>
